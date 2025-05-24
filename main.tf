cf9f0ffa8f883e7d4ae38d55d4183238024ebc85
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-24 13:44:58 -0400

    Added variables for sservice APIs

--- main.tf
+++ main.tf
@@ -2,3 +2,28 @@
 # 
 # Contains the main set of configuration for the module.
 #================================================
+
+resource "google_project_service" "api" {
+  project = var.project
+  service = "iam.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "api" {
+  for_each = toset(var.service_apis)
+  project = var.project
+  service = each.key
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_on_destroy = false
+}
