f7fc4261a3f037819047cd8af47b623cec133732
Bellanov Apilli <bellanovapilli@gmail.com>
2025-05-24 13:46:25 -0400

    Removed unparameteritized declaration

--- main.tf
+++ main.tf
@@ -4,18 +4,6 @@
 #================================================
 
 resource "google_project_service" "api" {
-  project = var.project
-  service = "iam.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "api" {
   for_each = toset(var.service_apis)
   project = var.project
   service = each.key
